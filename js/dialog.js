3b02821058b53d5890a3ffdc3b170cb35a228aca
Dmitry <bigvolkovski@gmail.com>
2020-10-05 15:36:20 +0300

    разделение кода на модули и добавление DnD

---
+++ js/dialog.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(() => {
+  const userDialog = document.querySelector(`.setup`);
+  const setupOpen = document.querySelector(`.setup-open`);
+  const setupClose = document.querySelector(`.setup-close`);
+  const setupUserName = document.querySelector(`.setup-user-name`);
+  const dialogHandle = userDialog.querySelector(`.upload`);
+
+  const defaultCoord = {
+    top: `80px`,
+    left: `50%`
+  };
+
+  const onPopupEscPress = (evt) => {
+    window.util.isEscEvent(evt, closePopup);
+  };
+
+  const closePopup = () => {
+    userDialog.classList.add(`hidden`);
+    document.removeEventListener(`keydown`, onPopupEscPress);
+    userDialog.style.left = defaultCoord.left;
+    userDialog.style.top = defaultCoord.top;
+  };
+
+  const openPopup = () => {
+    userDialog.classList.remove(`hidden`);
+    document.addEventListener(`keydown`, onPopupEscPress);
+  };
+
+  setupOpen.addEventListener(`click`, () => {
+    openPopup();
+  });
+
+  setupOpen.addEventListener(`keydown`, (evt) => {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener(`click`, () => {
+    closePopup();
+  });
+
+  setupClose.addEventListener(`keydown`, (evt) => {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  setupUserName.addEventListener(`focus`, () => {
+    document.removeEventListener(`keydown`, onPopupEscPress);
+  });
+
+  setupUserName.addEventListener(`blur`, () => {
+    document.addEventListener(`keydown`, onPopupEscPress);
+  });
+
+  window.dialog = {
+    userDialog,
+    dialogHandle
+  };
+})();
